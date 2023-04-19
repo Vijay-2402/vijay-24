022c852bd3148e2701861dd048676832b60a5ca7
Vijay-2402 <115980340+Vijay-2402@users.noreply.github.com>
2023-04-19 20:30:17 +0530

    Delete MvcProject directory

--- MvcProject/com/java/vijayConfig.java
+++
@@ -1,9 +0,0 @@
-package com.vijay;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-@Configuration
-@ComponentScan({"com.vijay"})
-public class vijayConfig 
-{
-  
-}
--- MvcProject/com/vijay/service/AddService.java
+++
@@ -1,7 +0,0 @@
-package com.vijay.service;
-
-public class AddService{
-  public int add(int i,int j) {
-	  return i+j;
-  }
-}
